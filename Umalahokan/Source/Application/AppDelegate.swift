7fe1c7a7138b3a30de71ce418802624b5e6ff3fe
Mounir Ybanez <rinuom91@gmail.com>
2017-01-30 14:23:03 +0800

    Added navbar's barTintColor, tintColor and barStyle.

--- Umalahokan/Source/Application/AppDelegate.swift
+++ Umalahokan/Source/Application/AppDelegate.swift
@@ -18,6 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let recentChat = RecentChatViewController()
         let nav = UINavigationController(rootViewController: recentChat)
         nav.navigationBar.isTranslucent = false
+        nav.navigationBar.barTintColor = UIColor(red: 57/255, green: 59/255, blue: 88/255, alpha: 1.0)
+        nav.navigationBar.barStyle = .black
+        nav.navigationBar.tintColor = UIColor(red: 250/255, green: 250/255, blue: 250/255, alpha: 1.0)
         window?.rootViewController = nav
         window?.makeKeyAndVisible()
         
