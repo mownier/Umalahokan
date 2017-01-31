7c8386ae4314aa8e310763362ccad63c7675620b
Mounir Ybanez <rinuom91@gmail.com>
2017-01-31 14:37:03 +0800

    Removed navigation contoller as root.

--- Umalahokan/Source/Application/AppDelegate.swift
+++ Umalahokan/Source/Application/AppDelegate.swift
@@ -15,16 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        let recentChat = RecentChatViewController()
-        let nav = UINavigationController(rootViewController: recentChat)
-        nav.navigationBar.isTranslucent = false
-        nav.navigationBar.barTintColor = UIColor(red: 57/255, green: 59/255, blue: 88/255, alpha: 1.0)
-        nav.navigationBar.barStyle = .black
-        nav.navigationBar.tintColor = UIColor(red: 250/255, green: 250/255, blue: 250/255, alpha: 1.0)
-        nav.navigationBar.titleTextAttributes = [
-            NSFontAttributeName: UIFont(name: "AvenirNext-Medium", size: 15)!
-        ]
-        window?.rootViewController = nav
+        window?.rootViewController = RecentChatViewController()
         window?.makeKeyAndVisible()
         
         return true
