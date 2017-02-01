3d1e27d4fe22492a878aed887cb9412e0aa8bba4
Mounir Ybanez <rinuom91@gmail.com>
2017-02-01 11:12:40 +0800

    Setup recent chat cell.

--- Umalahokan/Source/UI/Recent Chat/RecentChatViewController.swift
+++ Umalahokan/Source/UI/Recent Chat/RecentChatViewController.swift
@@ -17,12 +17,34 @@ class RecentChatViewController: UIViewController {
         rect.size = UIScreen.main.bounds.size
         recentChatView = RecentChatView(frame: rect)
         recentChatView.topBar.delegate = self
+        recentChatView.tableView.dataSource = self
+        recentChatView.tableView.rowHeight = 80
+        recentChatView.tableView.separatorStyle = .none
+        
+        RecentChatCell.register(in: recentChatView.tableView)
         
         view = recentChatView
     }
     
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+}
+
+extension RecentChatViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = RecentChatCell.dequeue(from: tableView)!
+        cell.moodLabel.text = "RELAXED"
+        cell.displayNameLabel.text = "Dominika Faniz"
+        cell.messageLabel.text = "Hey there. How is it going?"
+        cell.timeLabel.text = "now"
+        cell.selectionStyle = .none
+        return cell
     }
 }
 
