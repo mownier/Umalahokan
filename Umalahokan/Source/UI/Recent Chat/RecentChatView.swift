1cc94e346d987db9b2802ea518ca0849ed1ef54c
Mounir Ybanez <rinuom91@gmail.com>
2017-02-01 12:15:55 +0800

    Added setup and layout of composer button.

--- Umalahokan/Source/UI/Recent Chat/RecentChatView.swift
+++ Umalahokan/Source/UI/Recent Chat/RecentChatView.swift
@@ -12,6 +12,7 @@ class RecentChatView: UIView {
 
     var topBar: RecentChatTopBar!
     var tableView: UITableView!
+    var composerButton: UIButton!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -24,6 +25,7 @@ class RecentChatView: UIView {
     }
     
     override func layoutSubviews() {
+        let spacing: CGFloat = 8
         var rect = CGRect.zero
         
         rect.size = topBar.frame.size
@@ -33,14 +35,41 @@ class RecentChatView: UIView {
         rect.origin.y = rect.maxY
         rect.size.height = frame.height - rect.height
         tableView.frame = rect
+    
+        rect.size.width = 56
+        rect.size.height = rect.width
+        rect.origin.x = frame.width - rect.width - (spacing * 2)
+        rect.origin.y = frame.height - rect.height - (spacing * 2)
+        composerButton.layer.cornerRadius = rect.width / 2
+        composerButton.frame = rect
     }
     
     private func initSetup() {
         topBar = RecentChatTopBar()
+        
         tableView = UITableView()
         tableView.tableFooterView = UIView()
         
+        composerButton = UIButton()
+        composerButton.tintColor = UIColor.white
+        composerButton.clipsToBounds = true
+        composerButton.setImage(#imageLiteral(resourceName: "button-composer"), for: .normal)
+        composerButton.backgroundColor = UIColor(
+            red: 142/255,
+            green: 135/255,
+            blue: 251/255,
+            alpha: 1.0
+        )
+        composerButton.imageEdgeInsets.left = 4
+        composerButton.imageEdgeInsets.bottom = 4
+        composerButton.layer.shadowColor = UIColor.black.cgColor
+        composerButton.layer.shadowOffset = CGSize(width: 2, height: 2)
+        composerButton.layer.shadowRadius = 2
+        composerButton.layer.shadowOpacity = 0.5
+        composerButton.layer.masksToBounds = false
+        
         addSubview(topBar)
         addSubview(tableView)
+        addSubview(composerButton)
     }
 }
