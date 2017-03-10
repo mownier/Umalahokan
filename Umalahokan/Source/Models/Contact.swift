91701a98e287a085cb396489fc88fbd3e2ba9c37
Mounir Ybanez <rinuom91@gmail.com>
2017-03-10 14:39:17 +0800

    Removed isAnimatable and generateRandomList.

--- Umalahokan/Source/Models/Contact.swift
+++ Umalahokan/Source/Models/Contact.swift
@@ -13,18 +13,5 @@ struct Contact {
     var avatar: String = ""
     var name: String = ""
     var isOnline: Bool = false
-    var isAnimatable: Bool = false
-    
-    static func generateRandomList() -> [Contact] {
-        var list = [Contact]()
-        let count = arc4random() % 10 + 10
-        for _ in 0..<count {
-            var contact = Contact()
-            contact.name = "Merriam Webster"
-            contact.isOnline = arc4random() % 2 == 0 ? true : false
-            list.append(contact)
-        }
-        return list
-    }
 }
 
