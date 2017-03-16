aa5c4e020de901d551d645d822ca0fc6d50b260c
Mounir Ybanez <rinuom91@gmail.com>
2017-03-16 13:51:54 +0800

    Added LayoutDimension.

---
+++ Umalahokan/Source/Util/LayoutDimension.swift
@@ -0,0 +1,14 @@
+//
+//  LayoutDimension.swift
+//  Umalahokan
+//
+//  Created by Mounir Ybanez on 16/03/2017.
+//  Copyright © 2017 Ner. All rights reserved.
+//
+
+import CoreGraphics
+
+struct LayoutDimension {
+
+    let spacing: CGFloat = 8.0
+}
