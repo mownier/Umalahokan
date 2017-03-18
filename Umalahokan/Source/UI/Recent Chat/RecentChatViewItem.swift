09f289f51b9a63895937fbfcdd6f7a6e078f09eb
Mounir Ybanez <rinuom91@gmail.com>
2017-03-18 15:24:13 +0800

    Added configuration of recent chat view.

---
+++ Umalahokan/Source/UI/Recent Chat/RecentChatViewItem.swift
@@ -0,0 +1,37 @@
+//
+//  RecentChatViewItem.swift
+//  Umalahokan
+//
+//  Created by Mounir Ybanez on 18/03/2017.
+//  Copyright © 2017 Ner. All rights reserved.
+//
+
+import Kingfisher
+
+protocol RecentChatViewItem {
+
+    var isOnline: Bool { get }
+    var avatarUrl: URL? { get }
+}
+
+protocol RecentChatViewConfiguration {
+    
+    func configure(_ item: RecentChatViewItem?)
+}
+
+extension RecentChatView: RecentChatViewConfiguration {
+    
+    func configure(_ item: RecentChatViewItem?) {
+        guard let item = item else { return }
+        
+        if let url = item.avatarUrl {
+            let resource = ImageResource(downloadURL: url)
+            topBar.rightItem.kf.setImage(with: resource, placeholder: #imageLiteral(resourceName: "avatar-placeholder"), options: nil, progressBlock: nil, completionHandler: nil)
+            
+        } else {
+            topBar.rightItem.image = #imageLiteral(resourceName: "avatar-placeholder")
+        }
+        
+        topBar.onlineStatusIndicator.isHidden = !item.isOnline
+    }
+}
