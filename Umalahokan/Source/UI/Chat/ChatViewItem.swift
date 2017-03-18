e9a40fe121ff569953c6168d4fa7ad31b0232c4c
Mounir Ybanez <rinuom91@gmail.com>
2017-03-18 12:00:56 +0800

    Added configuratio of chat view.

---
+++ Umalahokan/Source/UI/Chat/ChatViewItem.swift
@@ -0,0 +1,50 @@
+//
+//  ChatViewItem.swift
+//  Umalahokan
+//
+//  Created by Mounir Ybanez on 18/03/2017.
+//  Copyright © 2017 Ner. All rights reserved.
+//
+
+import Kingfisher
+
+protocol ChatViewItem {
+
+    var isOnline: Bool { get }
+    var moodText: String { get }
+    var moodColor: UIColor { get }
+    var avatarUrl: URL? { get }
+    var displayNameText: String { get }
+}
+
+protocol ChatViewConfiguration {
+    
+    func configure(_ item: ChatViewItem?, isPrototype: Bool)
+}
+
+extension ChatView: ChatViewConfiguration {
+    
+    func configure(_ item: ChatViewItem?, isPrototype: Bool = false) {
+        guard let item = item else { return }
+        
+        if !isPrototype {
+            if let url = item.avatarUrl {
+                let resource = ImageResource(downloadURL: url)
+                topBar.avatarImageView.kf.setImage(with: resource)
+            
+            } else {
+                topBar.avatarImageView.image = nil
+            }
+        }
+        
+        topBar.displayNameLabel.text = item.displayNameText
+        topBar.moodLabel.text = item.moodText
+        topBar.moodIndicator.backgroundColor = item.moodColor
+        topBar.onlineStatusIndicator.isHidden = !item.isOnline
+        
+        topBar.setNeedsLayout()
+        topBar.layoutIfNeeded()
+    }
+}
+
+
