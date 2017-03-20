6ed3418bdad049c372f10644426fd9decc2144e6
Mounir Ybanez <rinuom91@gmail.com>
2017-03-20 15:10:31 +0800

    Added isValidToReload parameter.

--- Umalahokan/Source/UI/Chat/ChatView.swift
+++ Umalahokan/Source/UI/Chat/ChatView.swift
@@ -14,6 +14,7 @@ class ChatView: UIView {
     var collectionView: UICollectionView!
     var sendView: SendView!
     var flowLayout: UICollectionViewFlowLayout!
+    var isValidToReload: Bool = false
     
     convenience init() {
         self.init(frame: .zero)
