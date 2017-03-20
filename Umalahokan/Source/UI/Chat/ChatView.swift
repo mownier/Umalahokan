a8b3017c249e1788e3bdf8796ba1d9fc2ca47d59
Mounir Ybanez <rinuom91@gmail.com>
2017-03-20 14:15:16 +0800

    Transferred tap gesture to collectionView.

--- Umalahokan/Source/UI/Chat/ChatView.swift
+++ Umalahokan/Source/UI/Chat/ChatView.swift
@@ -42,12 +42,6 @@ class ChatView: UIView {
     func initSetup() {
         let spacing = LayoutDimension().spacing
         
-        let tap = UITapGestureRecognizer()
-        tap.numberOfTapsRequired = 1
-        tap.cancelsTouchesInView = false
-        tap.addTarget(self, action: #selector(UIView.endEditing(_:)))
-        addGestureRecognizer(tap)
-        
         backgroundColor = UIColor.white
         
         topBar = ChatTopBar()
@@ -59,6 +53,12 @@ class ChatView: UIView {
         collectionView.backgroundColor = UIColor.clear
         collectionView.contentInset.top = spacing
         collectionView.contentInset.bottom = spacing
+        
+        let tap = UITapGestureRecognizer()
+        tap.numberOfTapsRequired = 1
+        tap.cancelsTouchesInView = false
+        tap.addTarget(self, action: #selector(UIView.endEditing(_:)))
+        collectionView.addGestureRecognizer(tap)
         
         sendView = SendView()
         
