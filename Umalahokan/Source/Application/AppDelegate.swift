e85a012b88435b5ee59b0e6876f1985c3b3fd2d5
Mounir Ybanez <rinuom91@gmail.com>
2017-03-24 11:10:05 +0800

    Called abstract configuration of Firebase.

--- Umalahokan/Source/Application/AppDelegate.swift
+++ Umalahokan/Source/Application/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let root = UmalahokanDrawerController()
         window?.rootViewController = root
         window?.makeKeyAndVisible()
+        
+        FIRApp.configure()
         
         return true
     }
