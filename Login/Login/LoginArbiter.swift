155bc9b3a02c5dfae42705f735e77f6d3678b30a
Mounir Ybanez <rinuom91@gmail.com>
2017-04-05 14:56:19 +0800

    Added tests on LoginModule.

--- Login/Login/LoginArbiter.swift
+++ Login/Login/LoginArbiter.swift
@@ -7,42 +7,8 @@
 //
 
 import Viper
-import ServiceProvider
 
 public protocol LoginArbiter: Arbiter {
     
     func login(email: String, password: String)
-}
-
-public protocol LoginBuilder: Builder {
-    
-}
-
-public class LoginModule: Module, Interactable {
-    
-    public typealias ModulePresenter = LoginPresenter
-    public typealias ModuleWireframe = LoginWireframe
-    public typealias ModuleScene = LoginScene
-    public typealias ModuleInteractor = LoginInteractor
-    
-    public var presenter: LoginPresenter!
-    public var wireframe: LoginWireframe!
-    public var scene: LoginScene!
-    public var interactor: LoginInteractor!
-    
-    required public init(scene: LoginScene) {
-        self.scene = scene
-    }
-}
-
-extension LoginModule: LoginBuilder {
-    
-//    public func build(root: RootWireframe?) {
-//        guard let authService = AuthRemoteService() else { return }
-//        
-//        interactor = LoginInteractor(authService: authService)!
-//        wireframe = LoginWireframe()
-//        presenter = LoginPresenter()
-//        
-//    }
 }
--- Login/LoginTests/LoginSceneMock.swift
+++ Login/LoginTests/LoginSceneMock.swift
@@ -7,14 +7,19 @@
 //
 
 import Login
+import Viper
 
 class LoginSceneMock: LoginScene {
 
     var presenter: LoginArbiter!
     var errorMessage: String?
     var isShowingLoadView: Bool = false
-    
+
     func showLoginError(message: String) {
         errorMessage = message
+    }
+    
+    func setupArbiter<T : Arbiter>(_ arbiter: T) {
+        presenter = arbiter as! LoginArbiter
     }
 }
