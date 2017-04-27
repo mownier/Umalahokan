a3c011c52160ccbaacc1600a457687e9b9252b2d
Mounir Ybanez <rinuom91@gmail.com>
2017-04-27 15:42:43 +0800

    Added Core Models.

---
+++ Core/Core/Contact.swift
@@ -0,0 +1,14 @@
+//
+//  Contact.swift
+//  Core
+//
+//  Created by Mounir Ybanez on 27/04/2017.
+//  Copyright © 2017 Ner. All rights reserved.
+//
+
+public struct Contact {
+    
+    var avatar: String = ""
+    var name: String = ""
+    var isOnline: Bool = false
+}
---
+++ Core/Core/Mood.swift
@@ -0,0 +1,16 @@
+//
+//  Mood.swift
+//  Core
+//
+//  Created by Mounir Ybanez on 27/04/2017.
+//  Copyright © 2017 Ner. All rights reserved.
+//
+
+public enum Mood {
+    
+    case relaxed
+    case romantic
+    case neutral
+    case curious
+    case happy
+}
